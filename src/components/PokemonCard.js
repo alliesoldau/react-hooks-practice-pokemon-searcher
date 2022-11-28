74e98c1a0b8973f707d6f4232d1ca7dce508fafc
Allie Soldau <alliesoldau@gmail.com>
2022-11-28 12:02:37 -0500

    poke cards rendered, click event sprite flip working

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ poke }) {
+
+  const [imageSource, setImageSource] = useState(poke.sprites.front);
+
+  function handleSpriteFlip() {
+    if (imageSource === poke.sprites.front) {
+      setImageSource(poke.sprites.back)
+    } else {
+      setImageSource(poke.sprites.front);
+    }
+  }
+
   return (
     <Card>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div className="image" onClick={handleSpriteFlip}>
+          <img alt="oh no!" src={imageSource}/>
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{poke.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {poke.hp}
           </span>
         </div>
       </div>
